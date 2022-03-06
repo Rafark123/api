ae0a848f025574cde92c771de671bc63d0576c59
Rafark123 <zumbidoalasca@gmail.com>
2022-03-06 15:47:43 -0300

    comk

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 let a = 'nada'
 let b
+var port = process.env.PORT || 3000
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
@@ -25,4 +26,4 @@ app.get('/', (req, res) => {
 res.sendFile('R:\\API-Receber-Info\\index..html')
 });
 
-app.listen(8000);
+app.listen(port);

